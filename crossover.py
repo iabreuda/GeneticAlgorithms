7dfec20fd1b896f4dc1f91168a56f7fec2ea09f8
Jao Felipe <guedes.joaofelipe@poli.ufrj.br>
2019-07-08 16:27:15 -0300

    Improving mutation code to lambda functions

--- crossover.py
+++ crossover.py
@@ -1,8 +1,9 @@
 import numpy as np
 import population as p
 
-def onePointCrossOver(parent1, parent2):
+def onePointCrossOver(parent1, parent2, verbose=False):    
     position = np.random.randint(1, parent1.shape[0])
+    print ('Onepoint Crossover position: ', position) if verbose else None
     child1 = np.append(parent1[0:position], parent2[position:])
     child2 = np.append(parent2[0:position], parent1[position:])
     return child1, child2
@@ -10,7 +11,8 @@ def onePointCrossOver(parent1, parent2):
 def main():
     parent1 = p.initializePopulationReal(5, 0 ,5, 1)[0]
     parent2 =  p.initializePopulationReal(5, 0 ,5, 1)[0]
-    print(onePointCrossOver(parent1, parent2))
+    child1, child2 = onePointCrossOver(parent1, parent2, True)
+    print ('parent1: {}\nparent2: {}\nchild1: {}\nchild2: {}'.format(parent1, parent2, child1, child2))
 
 if __name__ == "__main__":
     main()        
--- mutation.py
+++ mutation.py
@@ -3,28 +3,18 @@ import population as p
 
 #Binary Representation
 def binaryMutation(parent, probability):
-    for row in range(0, parent.shape[0]):
-        for column in range(0, parent.shape[1]):
-            if (np.random.rand() < probability):
-                parent[row][column] = 1 - parent[row][column]
-    return parent
+    mutate = lambda x: 1-x if (np.random.rand() < probability) else x
+    return np.vectorize(mutate)(parent)
 
 #Integer Representation
-def IntegerRandomResetting(parent, probability, lower, upper):
-    for row in range(0, parent.shape[0]):
-        for column in range(0, parent.shape[1]):
-            if (np.random.rand() < probability):
-                parent[row][column] = np.random.randint(lower, upper)
-    return parent    
-
+def integerRandomResetting(parent, probability, lower, upper):
+    mutate = lambda x: np.random.randint(lower, upper) if (np.random.rand() < probability) else x
+    return np.vectorize(mutate)(parent)
 
 #Validar se depois da soma os valores passaram os limites da populacao
 def creepMutation(parent, probability, lower, upper):    
-    for row in range(0, parent.shape[0]):
-        for column in range(0, parent.shape[1]):
-            if (np.random.rand() < probability):
-                parent[row][column] = parent[row][column] + np.random.randint(lower, upper)
-    return parent    
+    mutate = lambda x: x + np.random.randint(lower, upper) if (np.random.rand() < probability) else x
+    return np.vectorize(mutate)(parent)
 
 #Float Representation
 def uniformMutation(parent, probability, lower, upper):
@@ -33,9 +23,11 @@ def uniformMutation(parent, probability, lower, upper):
             parent[i] = (upper - lower)*(np.random.rand()) + lower
     return parent        
 
-
-
 def main():
+    arr = np.array([[1, 0], [0, 1]])
+    probability = .8
+    print ('Binary mutation (prob={}): \n{} \n>> \n{}'.format(probability, arr, binaryMutation(arr, probability)))
+    print ('Integer resetting (prob={}): \n{} \n>> \n{}'.format(probability, arr, integerRandomResetting(arr, probability, 0, 5)))
     print(uniformMutation(p.initializePopulationReal(5, 0 ,5, 1), 0.5, 0, 5))
 
 if __name__ == "__main__":
