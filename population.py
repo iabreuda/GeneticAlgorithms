e59baf4816edb773ab03f628832659435e5cdefb
Igor Abreu <igorabreu.ds@poli.ufrj.br>
2019-07-08 10:17:00 -0300

    Adds methods to initialize population

---
+++ population.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def initializePopulationReal(dimension, lower, upper, initPopulation):
+    return (upper - lower)*(np.random.rand(dimension, initPopulation)) + lower
+
+def initializePopulationBin(dimension, initPopulation):
+    return np.random.randint(0, 2, size=(initPopulation, dimension))
+
+def initializerPopulationInteger(dimension, lower, upper, initPopulation):
+    return np.random.randint(lower, upper, size=(initPopulation, dimension))
+
+def main():
+    print(initializePopulationBin(5, 5))
+
+if __name__ == "__main__":
+    main()    
