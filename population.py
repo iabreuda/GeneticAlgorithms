0a7a47b76772e200bd82b2799dc52c66d9f29694
Igor Abreu <igorabreu.ds@poli.ufrj.br>
2019-07-15 10:47:25 -0300

    Create class to handle population

--- population.py
+++ population.py
@@ -1,16 +1,41 @@
 import numpy as np
 
-def initializePopulationReal(dimension, lower, upper, initPopulation):
-    return (upper - lower)*(np.random.rand(initPopulation, dimension)) + lower
+class Population:
 
-def initializePopulationBin(dimension, initPopulation):
-    return np.random.randint(0, 2, size=(initPopulation, dimension))
+    def __init__(self, dimension, lowerLimit, upperLimit, initialPopulation):
+        self.dimension = dimension
+        self.lowerLimit = lowerLimit
+        self.upperLimit = upperLimit
+        self.initialPopulation = initialPopulation
 
-def initializerPopulationInteger(dimension, lower, upper, initPopulation):
-    return np.random.randint(lower, upper, size=(initPopulation, dimension))
+    def getDimension(self):
+        return self.dimension
 
-def main():
-    print(initializePopulationReal(dimension=10, lower=-100, upper=100, initPopulation=2))
+    def setDimentison(self, dimension):
+        self.dimension = dimension
+
+    def getLowerLimit(self):
+        return self.lowerLimit
+
+    def setLowerLimit(self, lowerLimit):
+        self.lowerLimit = lowerLimit
+
+    def getUpperLimit(self):
+        return self.upperLimit
+
+    def setUpperLimit(self, upperLimit):
+        self.upperLimit = upperLimit
+
+    def getInitialPopulation(self):
+        return self.initialPopulation
+
+    def setInitialPopulation(self, initialPopulation):
+        self.initialPopulation = initialPopulation
+
+    def createPopulation(self):
+        return (self.getUpperLimit() - self.getLowerLimit())*(np.random.rand(self.initialPopulation, self.dimension)) + self.lowerLimit
+
 
 if __name__ == "__main__":
-    main()    
+    pop = Population(dimension=10, lowerLimit=-100, upperLimit=100, initialPopulation=2)
+    print("Initial Population:\n", pop.createPopulation())
