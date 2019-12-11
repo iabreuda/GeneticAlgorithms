659610426ddeb58d04e49d50914d6b4c0a72e15b
Igor Abreu <igorabreu.ds@poli.ufrj.br>
2019-12-11 06:49:47 -0300

    Class to handle pop

--- population.py
+++ population.py
@@ -1,9 +1,8 @@
 import numpy as np
-import individual as i
 
-class Population:
+class Population(object):
 
-    def __init__(self, dimension, lowerLimit, upperLimit, initialPopulation):
+    def __init__(self, individuals, generation):
         """Create a population sample
 
         Arguments:
@@ -12,92 +11,31 @@ class Population:
             upperLimit {float} -- [Upper limit]
             initialPopulation {integer} -- [Number of individuals in initial populaton]
         """
-        self.dimension = dimension
-        self.lowerLimit = lowerLimit
-        self.upperLimit = upperLimit
-        self.initialPopulation = initialPopulation
-        self.population = []
-        self.setPopulation()
+        self.individuals = individuals
+        self.generation = generation
 
-    def getDimension(self):
-        """Current number of dimensions
+    def getGeneration(self):
+        return self.generation
 
-        Returns:
-            [Integer] -- [Number of problem dimension]
-        """
-        return self.dimension
+    def setGeneration(self, generation):
+        self.generation = generation
 
-    def setDimentison(self, dimension):
-        """Define a new number of dimensions
+    def getIndividuals(self):
+        return self.individuals
 
-        Arguments:
-            dimension {integer} -- [Number of elements in chromosome array]
-        """
-        self.dimension = dimension
+    def setIndividuals(self, individuals):
+        self.individuals = individuals
 
-    def getLowerLimit(self):
-        """Get Inferior limit
+    def getGenerationalStatistics(self):
+        fitness = []
+        for individuo in self.individuals:
+            fitness.append(individuo.getFitness())
+        np_arr = np.array(fitness)
 
-        Returns:
-            [float] -- inferior limit
-        """
-        return self.lowerLimit
+        best = np.amax(np_arr)
+        avarage = np.mean(np_arr)
+        worst = np.amin(np_arr)
+        median = np.median(np_arr)
+        std = np.std(np_arr)
 
-    def setLowerLimit(self, lowerLimit):
-        """Define a new lower limit
-
-        Arguments:
-            lowerLimit {float} -- [New lower limit]
-        """
-        self.lowerLimit = lowerLimit
-
-    def getUpperLimit(self):
-        """Get superior limit
-
-        Returns:
-            [float] -- superior limit
-        """
-        return self.upperLimit
-
-    def setUpperLimit(self, upperLimit):
-        """Define a new upper limit
-
-        Arguments:
-            upperLimit {float} -- [New upper limit]
-        """
-        self.upperLimit = upperLimit
-
-    def getInitialPopulation(self):
-        """Get number of individuals in initial population
-
-        Returns:
-            [integer] -- number of individuals in initial population
-        """
-        return self.initialPopulation
-
-    def setInitialPopulation(self, initialPopulation):
-        """Define size of initial population
-
-        Arguments:
-            initialPopulation {integer} -- Size of initial population
-        """
-        self.initialPopulation = initialPopulation
-
-    def setPopulation(self):
-        """Create a random population using parameters of this class
-        """
-        population = (self.getUpperLimit() - self.getLowerLimit())*(np.random.rand(self.initialPopulation, self.dimension)) + self.lowerLimit
-        individuals = []
-        for chromosome in population:
-            individual = i.Individual(chromosome)
-            individuals.append(individual)
-        self.population = individuals
-
-    def getPopulation(self):
-        """Get a population
-
-        Returns:
-            [Array] -- Array of individuals
-        """
-        return self.population
-
+        print("Gen: ", self.generation, " Best: ", best, " Worst: ", worst, " Avg: ", avarage , " Median: ", median, " Std:", std)
